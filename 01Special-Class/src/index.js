6d681ac9158240b2450033d1b7021c8cbc8df23b
Nikhil Borse <nikhilrb13@gmail.com>
2023-09-15 15:14:41 +0530

    error fixed

--- 01Special-Class/src/index.js
+++ 01Special-Class/src/index.js
@@ -1,33 +1,34 @@
 import app from "./app";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
 const port = 3000;
 
 // database connection - mongodb
 // by using iife
 // one professional approach you can say it
-(async () => {
-  try {
-    // db connection
-    await mongoose.connect("dbstring");
 
-    app.on("error", (err) => {
-      console.log(`error :`, err);
-      throw err;
-    });
+// (async () => {
+//   try {
+//     // db connection
+//     await mongoose.connect("dbstring");
 
-    const onListening = () => {
-      console.log(`listening on port ${port}`);
-    };
+//     app.on("error", (err) => {
+//       console.log(`error :`, err);
+//       // throw err;
+//     });
 
-    app.listen(port, onListening);
-  } catch (err) {
-    // catch block
-    console.error("Error :", err);
-    throw err;
-  }
-})();
+//     const onListening = () => {
+//       console.log(`listening on port ${port}`);
+//     };
 
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
+//     app.listen(port, onListening);
+//   } catch (err) {
+//     // catch block
+//     console.error("Error :", err);
+//     // throw err;
+//   }
+// })();
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
