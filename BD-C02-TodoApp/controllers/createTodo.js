f31fd618e3cbcaf6e37c1452b4a7caeb0d96504f
Nikhil Borse <nikhilrb13@gmail.com>
2023-10-04 17:59:25 +0530

    BD-02 Todoapp

---
+++ BD-C02-TodoApp/controllers/createTodo.js
@@ -0,0 +1,11 @@
+// import the model
+const todo = require("../models/Todo");
+
+// define route handler
+exports.createTodo=async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
