f5578929afb8ef642561a986dcf74cc0d92feaf3
Nikhil Borse <nikhilrb13@gmail.com>
2023-10-05 19:02:13 +0530

    router define and mapped

--- BD-C02-TodoApp/controllers/createTodo.js
+++ BD-C02-TodoApp/controllers/createTodo.js
@@ -1,11 +1,30 @@
 // import the model
-const todo = require("../models/Todo");
+const Todo = require("../models/Todo");
 
 // define route handler
-exports.createTodo=async (req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+exports.createTodo = async (req, res) => {
+  try {
+    // extract title and description from req body
+    /** yaha se data fetch kiya */
+    const { title, description } = req.body;
+
+    // yaha se data insert kiya
+    // create new todo object and insert in db
+    const response = await Todo.create({ title, description });
+
+    // send json response with success flag
+    res.status(200).json({
+      success: true,
+      data: response,
+      message: "Entry created successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      data: "internal server error",
+      message: error.message,
+    });
+  }
+};
--- BD-C02-TodoApp/index.js
+++ BD-C02-TodoApp/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+// load config  from env file
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+// middleware to parse json request body
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log(`server running on port 3000`);
 });
